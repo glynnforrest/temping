b90964f692dcb427f4ca016e8cb50e8f615cd2a6
glynnforrest <me@glynnforrest.com>
2013-07-18 23:51:30 +0100

    Adding bare functionality for creating the temporary directory.

--- src/Temping/Temping.php
+++ src/Temping/Temping.php
@@ -9,9 +9,53 @@ namespace Temping;
  **/
 class Temping {
 
-	public function __construct($directory = null) {
+	const temping_dir_name = 'php-temping/';
 
+	protected static $instance;
+
+	//path to the temporary directory where all Temping files are
+	//created.
+	protected $dir;
+
+	protected function __construct() {
 	}
+
+	/**
+	 * Get an instance of Temping, ensuring a temporary directory has
+	 * been created.
+	 */
+	public static function getInstance() {
+		if(!self::$instance) {
+			self::$instance = new self();
+		}
+		self::$instance->init();
+		return self::$instance;
+	}
+
+	/**
+	 * Create the temporary directory if it doesn't exist.
+	 */
+	protected function init() {
+		$directory = sys_get_temp_dir();
+		if(substr($directory, -1) !== '/') {
+			$directory .= '/';
+		}
+		$this->dir = $directory . self::temping_dir_name;
+		if(!file_exists($this->dir)) {
+			mkdir($this->dir);
+		}
+	}
+
+	/**
+	 * Delete the temporary directory created by Temping and all files
+	 * within it.
+	 */
+	public function destroy() {
+		if(file_exists($this->dir)) {
+			rmdir($this->dir);
+		}
+	}
+
 
 
 }
--- tests/Temping/Tests/TempingTest.php
+++ tests/Temping/Tests/TempingTest.php
@@ -13,11 +13,36 @@ include(__DIR__ . '/../../bootstrap.php');
  **/
 class TempingTest extends \PHPUnit_Framework_TestCase {
 
-	public function testConstruct() {
-		$temp = new Temping();
+	protected function createFilePath($filename) {
+		$tmp_dir = sys_get_temp_dir();
+		if(substr($tmp_dir, -1) !== '/') {
+			$tmp_dir .= '/';
+		}
+		return $tmp_dir . Temping::temping_dir_name . $filename;
+	}
+
+	public function testGetInstance() {
+		$temp = Temping::getInstance();
 		$this->assertTrue($temp instanceof Temping);
-		$temp_again = new Temping('/some/random/dir');
-		$this->assertTrue($temp_again instanceof Temping);
+	}
+
+	public function testTempingDirCreatedOnConstruct() {
+		$temp = Temping::getInstance();
+		$this->assertFileExists($this->createFilePath(null));
+	}
+
+	public function testTempingDirRemoved() {
+		$temp = Temping::getInstance();
+		$temp->destroy();
+		$this->assertFileNotExists($this->createFilePath(null));
+	}
+
+	public function testTempingDirRecreatedAfterDestroy() {
+		$temp = Temping::getInstance();
+		$temp->destroy();
+		$this->assertFileNotExists($this->createFilePath(null));
+		$another_instance = Temping::getInstance();
+		$this->assertFileExists($this->createFilePath(null));
 	}
 
 }
